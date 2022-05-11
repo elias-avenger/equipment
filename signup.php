608e8671d26ede894268837555468549aed0c61c
jonathan otiakori <otiakorijonathan4@gmail.com>
2022-05-11 17:21:13 +0300

    added a styling to signup.php

--- signup.php
+++ signup.php
@@ -15,29 +15,44 @@
   <head>
     <meta charset="utf-8">
     <title>O&P-UG</title>
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.6.1/dist/css/bootstrap.min.css">
+  <script src="https://cdn.jsdelivr.net/npm/jquery@3.6.0/dist/jquery.slim.min.js"></script>
+  <script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.1/dist/umd/popper.min.js"></script>
+  <script src="https://cdn.jsdelivr.net/npm/bootstrap@4.6.1/dist/js/bootstrap.bundle.min.js"></script>
   </head>
   <body>
+  <div class="col-md-12"><center>
+  <div class="col-md-4"> 
+
+<h4 class="alert bg-info text-white text-center"> sign up as an administrator</h4> 
+  <div class="card" >
+    <div class ="card-header"><h5>sign up!</h5></div>
+  <div class="card-body center p2-rounded shadow" >
     <form class="" action="insert_signup.php" method="post">
-      <br><br>
-      Full Name:
-      <input type="text"4 name="name">
-      <br><br>
-      Phone Number:
-      <input type="Phone" name="phone">
-      <br><br>
-      Email:
-      <input type="email" name="email">
-      <br><br>
+      <br>
+
       
-      Password:
-      <input type="password" name="pass">
-      <br><br>
-      Confirm Password:
-      <input type="password" name="">
-      <br><br>
+      <input class="form-control"  type="text"4 name="name" placeholder="enter name:">
+      <br>
+      
+      <input class="form-control" type="Phone" name="phone" placeholder="Phone Number:">
+      <br>
+     
+      <input class="form-control" type="email" name="email" placeholder=" Email:">
+      <br>
+      
+      <input class="form-control" type="password" name="pass" placeholder="Password:
+">
+      <br>
+     
+      <input class="form-control" type="password" name="" placeholder="Confirm Password:">
+      <br>
        
-      <input type="submit" value="SignUp">
+      <input class="btn btn-primary"  type="submit" value="SignUp">
     </form>
+    </div></div>
+    </div> 
+      </div></center>
   </body>
 </html>
  
