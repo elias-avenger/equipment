2bd9b190bbee6814ead6194670380aa53c2a6780
jonathan otiakori <otiakorijonathan4@gmail.com>
2022-05-17 17:44:58 +0300

    added bootstrap forms to admin

--- insert_signup.php
+++ insert_signup.php
@@ -24,11 +24,11 @@
   VALUES('$n', '$p', '$e', '$pwd', '$t')") or die("Failed to add user: ".mysqli_error($conn));
   print $n." successfully added as: <b>".$type;
   if(isset($_SESSION['email'])){
-    $q = mysqli_query($conn, "SELECT id FROM user WHERE email='$e'");
-    $r = mysqli_fetch_array($q);
-    $id = $r['id'];
-    $n_pwd = $n[0].$n[1].$n[2].$id;
-    mysqli_query($conn, "UPDATE user SET password = '$n_pwd' WHERE id = '$id'") or die("Failed to update pass: ".mysqli_error($conn));
-    print " </b><br>...<br>The user's password is: <b>".$n_pwd;
+      $q = mysqli_query($conn, "SELECT id FROM user WHERE email='$e'");
+      $r = mysqli_fetch_array($q);
+      $id = $r['id'];
+      $n_pwd = $n[0].$n[1].$n[2].$id;
+      mysqli_query($conn, "UPDATE user SET password = '$n_pwd' WHERE id = '$id'") or die("Failed to update pass: ".mysqli_error($conn));
+      print " </b><br>...<br>The user's password is: <b>".$n_pwd;
   }
 ?>
